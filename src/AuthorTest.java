c5bf50067fba315045876f70b82ce06d80e63aad
fuzi03 <113728947+fuzi03@users.noreply.github.com>
2022-09-27 18:57:37 +0300

    Delete AuthorTest.java

--- src/AuthorTest.java
+++
@@ -1,7 +0,0 @@
-public class AuthorTest {
-    public static void main(String[] args) {
-        Author author = new Author("Darya", "darya@mail.ru", 'A');
-        author.setEmail("darya@mail.ru");
-        System.out.print(author);
-    }
-}
