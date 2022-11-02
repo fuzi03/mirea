abb7e711d409d90d85044113cf73ecd8d4746718
fuzi03 <113728947+fuzi03@users.noreply.github.com>
2022-11-02 18:18:30 +0300

    Delete task13 directory

--- task13/task13_punkt2_Student.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class task13_punkt2_Student implements Comparator<task13_punkt2> {
-
-    @Override
-    public int compare(task13_punkt2 o1, task13_punkt2 o2) {
-        return o2.getA_score() - o1.getA_score();
-    }
-}
